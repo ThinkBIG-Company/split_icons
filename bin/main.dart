34ea505168ce913a3f0dcf61042ce6a3753004ed
ThinkBIG-Company <developers@think-big-company.com>
2021-05-12 05:04:20 +0200

    dev

--- bin/main.dart
+++ bin/main.dart
@@ -1,57 +1,71 @@
 import 'dart:io';
-import 'package:yaml/yaml.dart';
-import 'package:io/ansi.dart';
 
-import 'temp.dart';
+import 'package:io/ansi.dart';
+import 'package:yaml/yaml.dart';
+
+import 'yaml_modify.dart';
 
 void main(List<String> arguments) {
   // Platform.environment.forEach((k,v)=>print('$k $v'));
   // 执行命令
   // Process.run(executable, arguments);
-  print(green.wrap('${DateTime.now()}: The command is executing, please wait...'));
+  print(yellow.wrap(
+      '${DateTime.now().toString()
+          .substring(0, 19)}: The command is executing, please wait...'));
   var pubSpec = File('./pubspec.yaml');
+  //print(pubSpec.existsSync());
   var pubSpecDoc = loadYaml(pubSpec.readAsStringSync());
   var packages = File('./.packages');
   if (!packages.existsSync()) {
-    print(yellow.wrap('${DateTime.now()}: Get the dependencies first'));
+    print(yellow.wrap('${DateTime.now().toString()
+        .substring(0, 19)}: Get the dependencies first'));
     return;
   }
   // ignore: omit_local_variable_types
   List<String> lines = packages.readAsLinesSync();
-  // flutter_icons:file:///Users/makisu/development/flutter/.pub-cache/hosted/pub.flutter-io.cn/flutter_icons-1.0.0+1/lib/
   String flutterIconPath;
   var flutterIconVersion;
   for (var i = 0; i < lines.length; i++) {
     if (lines[i].contains('flutter_icons')) {
       flutterIconPath = lines[i]
-          .replaceAll("flutter_icons:file://${Platform.isWindows?'/':''}", '')
+          .replaceAll(
+          'flutter_icons:file://${Platform.isWindows ? '/' : ''}', '')
           .replaceAll('/lib/', '');
-      //flutterIconVersion = flutterIconPath.substring(flutterIconPath.lastIndexOf('-'));
-      flutterIconVersion = '';
+
+      if (flutterIconVersion != null) {
+        flutterIconVersion =
+            flutterIconPath.substring(flutterIconPath.lastIndexOf('-'));
+      }
+
       break;
     }
   }
-  if (flutterIconPath == null) {
-    print(yellow
-        .wrap('${DateTime.now()}: Get the flutter_icons dependency first'));
+
+  if (flutterIconPath == null || !File(flutterIconPath).existsSync()) {
+    print(red.wrap(
+        '${DateTime.now().toString()
+            .substring(0, 19)}: Attention! You need to add the flutter_icons dependency first'));
     return;
   }
   // ignore: omit_local_variable_types
-  File flutterIconsFile = File(flutterIconPath + '/pubspec.yaml');
+  File flutterIconsFile = File(flutterIconPath + 'pubspec.yaml');
   List includes;
   var yamlString = tempYaml(flutterIconVersion);
   if (pubSpecDoc['flutter_icons'] != null &&
       pubSpecDoc['flutter_icons']['includes'] != null) {
     includes = pubSpecDoc['flutter_icons']['includes'];
-    includes.forEach((key){
+    includes.forEach((key) {
       yamlString += fontsMap[key];
     });
-  }else{
+  } else {
     yamlString += allFonts;
   }
   flutterIconsFile.writeAsStringSync(yamlString);
   var resDoc = loadYaml(yamlString);
-  var useFonts = List.of(resDoc['flutter']['fonts']).map((item)=>item['family']);
-  print('${DateTime.now()}: The font you are using is $useFonts');
-  print(green.wrap('${DateTime.now()}: Finish the work'));
+  var useFonts = List.of(resDoc['flutter']['fonts']).map((
+      item) => item['family']);
+  print('${DateTime.now().toString()
+      .substring(0, 19)}: The fonts you are using is ${useFonts.toString()}');
+  print(blue.wrap('${DateTime.now().toString()
+      .substring(0, 19)}: Finish the work'));
 }
